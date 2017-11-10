d969cbc15d2e8a23a597544127d72873247a2006
worachai3 <worachai3@live.com>
2017-11-10 11:55:34 +0700

    control pacman

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -1,6 +1,11 @@
 package com.mygdx.game;
 
+import java.awt.RenderingHints.Key;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
@@ -8,17 +13,43 @@ public class GameScreen extends ScreenAdapter {
 	
 	private PacmanGame pacmanGame;
 	private Texture pacmanImg;
+	private int x;
+	private int y;
 	
 	public GameScreen(PacmanGame pacmanGame) {
 		this.pacmanGame = pacmanGame;
 		pacmanImg = new Texture("pacman.png");
+		
+		x = 100;
+		y = 100;
 	}
 	
 	@Override
 	public void render(float delta) {
+		update(delta);
+		
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
 		SpriteBatch batch = pacmanGame.batch;
 		batch.begin();
-		batch.draw(pacmanImg, 100, 100);
+		batch.draw(pacmanImg, x, y);
 		batch.end();
+	}
+
+	public void update(float delta) {
+		if(Gdx.input.isKeyPressed(Keys.LEFT)) {
+			x -= 10;
+		}
+		if(Gdx.input.isKeyPressed(Keys.RIGHT)) {
+			x += 10;
+		}
+		if(Gdx.input.isKeyPressed(Keys.DOWN)) {
+			y -= 10;
+		}
+		if(Gdx.input.isKeyPressed(Keys.UP)) {
+			y += 10;
+		}
+		
 	}	
 }
